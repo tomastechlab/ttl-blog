5836fc74c3e78100948552022c5e8d394e10185a
Tomas Schmidt <t.schmidt@codezilla.consulting>
2025-01-17 13:33:16 +0100

    move back to rsync as security flaws are fixed and deployment is working again

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 HOST=homelab
 DIR=/home/m900/share/server/tomastechlab.com/   # the directory where your web site files should go
 
-HUGO_ENV=production hugo && scp -r public/ ${HOST}:${DIR} # this will delete everything on the server that's not in the local public folder
+HUGO_ENV=production hugo && rsync -rzv public/* ${HOST}:${DIR} # this will delete everything on the server that's not in the local public folder
 
 exit 0
